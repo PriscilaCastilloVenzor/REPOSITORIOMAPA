0d8a7fff78b902d0c3c06fa315694b9bd68de6b1
PriscilaCastilloVenzor <102831942+PriscilaCastilloVenzor@users.noreply.github.com>
2022-04-01 16:01:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-adf = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/quantum-apps/mapa/main/data.csv")
 st.write(df)
 st.map(df)
